16d93d612a5d772848c55245a36301083298ab62
alegomeza <a.gomeza@hotmail.com>
2024-11-06 21:47:21 -0500

    Faltaron los comentarios

--- feet to meters.py
+++ feet to meters.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from tkinter import ttk
 
+
 def calculate(*args):
     try:
         value = float(feet.get())
@@ -8,31 +9,41 @@ def calculate(*args):
     except ValueError:
         pass
 
+
+# Configura la ventana principal de la aplicación
+# se le asigna el título "Feet to Meters"
 root = Tk()
 root.title("Feet to Meters")
 
+# Se crea un widget de frame que contendrá la interfaz de usuario
 mainframe = ttk.Frame(root, padding="3 3 12 12")
 mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
 root.columnconfigure(0, weight=1)
 root.rowconfigure(0, weight=1)
 
+# Entrada para ingresar el número de píes
 feet = StringVar()
 feet_entry = ttk.Entry(mainframe, width=7, textvariable=feet)
 feet_entry.grid(column=2, row=1, sticky=(W, E))
 
+# Entrada para ingresar el número de píes
 meters = StringVar()
 ttk.Label(mainframe, textvariable=meters).grid(column=2, row=2, sticky=(W, E))
 
-ttk.Button(mainframe, text="Calculate", command=calculate).grid(column=3, row=3, sticky=W)
+ttk.Button(mainframe, text="Calculate", command=calculate).grid(
+    column=3, row=3, sticky=W)
 
 ttk.Label(mainframe, text="feet").grid(column=3, row=1, sticky=W)
 ttk.Label(mainframe, text="is equivalent to").grid(column=1, row=2, sticky=E)
 ttk.Label(mainframe, text="meters").grid(column=3, row=2, sticky=W)
 
-for child in mainframe.winfo_children(): 
+for child in mainframe.winfo_children():
     child.grid_configure(padx=5, pady=5)
 
+# Con focus, el cursos inicia en este campo
 feet_entry.focus()
+# Se ejecuta calculate cuando se presione Return (Enter)
 root.bind("<Return>", calculate)
 
+# Iniciar el bucle de eventos
 root.mainloop()
